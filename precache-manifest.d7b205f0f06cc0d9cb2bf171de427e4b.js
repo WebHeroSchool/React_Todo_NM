7626005de0be1b9d259b7cc358fb896a8ee2b30d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-20 02:13:20 +0100

    Updates

---
+++ precache-manifest.d7b205f0f06cc0d9cb2bf171de427e4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd6ad065e2b08634581724348098ddf",
+    "url": "/React_TODO_NM/index.html"
+  },
+  {
+    "revision": "40699c94bd2dbc3f199e",
+    "url": "/React_TODO_NM/static/css/main.9f206ee0.chunk.css"
+  },
+  {
+    "revision": "b9047658e2cb757109aa",
+    "url": "/React_TODO_NM/static/js/2.ab822ce6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_TODO_NM/static/js/2.ab822ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40699c94bd2dbc3f199e",
+    "url": "/React_TODO_NM/static/js/main.bc8e1891.chunk.js"
+  },
+  {
+    "revision": "c063ec2711bd4d0ce4fc",
+    "url": "/React_TODO_NM/static/js/runtime-main.6ea0822b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_TODO_NM/static/media/logo.5d5d9eef.svg"
+  }
+]);

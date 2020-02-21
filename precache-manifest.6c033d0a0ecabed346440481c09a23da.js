2b87c79567f1971d3601e322fe0edc5bbdabfd0d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-21 19:05:04 +0100

    Updates

---
+++ precache-manifest.6c033d0a0ecabed346440481c09a23da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4592da33cfd27b0381b2cdfefa801e15",
+    "url": "/React_TODO_NM/index.html"
+  },
+  {
+    "revision": "54addd914219e59c3a31",
+    "url": "/React_TODO_NM/static/js/2.036ee636.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_TODO_NM/static/js/2.036ee636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9139ffe8832c266bac",
+    "url": "/React_TODO_NM/static/js/main.1f121d90.chunk.js"
+  },
+  {
+    "revision": "c063ec2711bd4d0ce4fc",
+    "url": "/React_TODO_NM/static/js/runtime-main.6ea0822b.js"
+  }
+]);

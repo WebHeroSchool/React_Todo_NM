7f0888209eb298c50fc59b61c3015a37eec9614d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-27 20:42:11 +0100

    project-structure_lesson18

---
+++ src/Components/InputItem/InputItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputItem = () => (
+    <div><span> Input Item </span></div>
+    );
+
+export default InputItem;
---
+++ src/Components/Item/Items.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Items = () => (
+    <span> Very important thing to do !!! </span>
+);
+
+export default Items;

c5d09a36c4271eebce7577820880dea4866015b9
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-28 19:41:18 +0100

    props_lesson19

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,13 +3,13 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
-
+const todoItem = 'Create the new app ';
 const App = () => (
     <div>
         <h1>Important TO-DO List</h1>
         <InputItem />
-        <ItemList />
-        <Footer />
+        <ItemList todoItem={todoItem}/>
+        <Footer  count={3}/>
     </div>
 );
 

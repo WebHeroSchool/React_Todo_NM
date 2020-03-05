d7cb1a45c852600950cc00318f8504420b53bfef
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-05 21:29:19 +0100

    some fixes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ItemList from '../ItemList/ItemList';
-import InputItem from '../InputItem/InputItem';
-import Footer from '../Footer/Footer';
+import ItemList from '/Components/ItemList/ItemList';
+import InputItem from '/Components/InputItem/InputItem';
+import Footer from '/Components/Footer/Footer';
 import styles  from './App.module.css';
 import { Container } from '@material-ui/core';
 

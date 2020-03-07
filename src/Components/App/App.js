08233fc4f25dfb4f1d376559dffb5ffe5a0f8b22
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-07 01:35:16 +0100

    component Class Lesson 27

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,37 +5,37 @@ import Footer from '../Footer/Footer';
 import styles  from './App.module.css';
 import { Container } from '@material-ui/core';
 
+class App extends React.Component{
+    render() {
+        const items = [
+            {
+                value: 'Create the new app ',
+                isDone: true
+            },
+            {
+                value: 'Insert props',
+                isDone: false
+            },
+            {
+                value: 'Do all the tasks',
+                isDone: true
+            },
+        ];
 
-const App = () => {
-
-    const items = [
-        {
-            value: 'Create the new app ',
-            isDone: true
-        },
-        {
-            value: 'Insert props',
-            isDone: false
-        },
-        {
-            value: 'Do all the tasks',
-            isDone: true
-        },
-    ];
-
-    return (
-        <Container fixed>
-            <div className={styles.wrap}>
+        return (
+            <Container fixed>
+                <div className={styles.wrap}>
                     <h1 className={styles.title}> TO DO List:</h1>
                     <InputItem />
-                <div>
-                    <ItemList items={items}/>
+                    <div>
+                        <ItemList items={items}/>
+                    </div>
+                    <Footer  count={3}/>
                 </div>
-                <Footer  count={3}/>
-            </div>
-        </Container>
-    );
-};
+            </Container>
+        );
+    }
+}
 
 
 export default App;

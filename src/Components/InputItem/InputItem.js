ff3a366297961a06e5083ed05e2e5342c7202cc0
Natalya Myunster <natalyamyunster@yahoo.com>
2020-03-08 18:40:01 +0100

    fixed clearInputHandler

--- src/Components/InputItem/InputItem.js
+++ src/Components/InputItem/InputItem.js
@@ -2,20 +2,55 @@ import React from "react";
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import styles from './InputItem.module.css';
+import ItemList from "../ItemList/ItemList";
 
 
 class InputItem extends React.Component{
     state ={
+        inputArray: <ItemList />,
         inputValue: '',
         helperText: '',
         error: false
     };
 
+
+    // addItem = () => {
+    //     let newValue = this.state.inputValue;
+    //     let newArray = this.state.inputArray;
+    //
+    //
+    //     if (newArray.includes(newValue)) {
+    //         this.setState({helperText: 'Already exist', error:true});
+    //     } else {
+    //         newValue === '' ?
+    //             this.setState({helperText: 'This field is requiered', error: true}) :
+    //             this.props.onClickAdd(this.state.inputValue);
+    //     }
+        // this.setState(previousState => ({
+        //     inputArray: [...previousState.inputArray, newValue]
+        // }, () =>  console.log(this.state.inputArray)));
+
+    // };
+
+
     clearInputHandler = () => {
-        this.state.inputValue === '' ?
-            this.setState({helperText: 'This field is requiered', error:true}) :
-            this.setState({inputValue : ''});
-        this.props.onClickAdd( this.state.inputValue );
+        // newValue === ''  ?
+        //     this.setState({helperText: 'This field is requiered', error:true}) :
+        // this.props.onClickAdd( this.state.inputValue );
+        let newValue = this.state.inputValue;
+        let newArray = this.state.inputArray;
+
+
+        if (newArray.includes(newValue)) {
+            this.setState({helperText: 'Already exist', error: true});
+        } else {
+            newValue === '' ?
+                this.setState({helperText: 'This field is requiered', error: true}) :
+                this.props.onClickAdd(this.state.inputValue);
+
+            this.setState({inputValue: ''});
+        }
+
     };
 
     render(){

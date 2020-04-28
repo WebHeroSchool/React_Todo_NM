f3dff0ce40d09583bf8690357b91d1d24531600d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-04-28 16:45:51 +0200

    class changed to hooks in component InputItem (need to fix label)

--- src/Components/Todo/InputItem/InputItem.js
+++ src/Components/Todo/InputItem/InputItem.js
@@ -1,58 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import styles from './InputItem.module.css';
 import propTypes from 'prop-types';
 
-class InputItem extends React.Component {
-    state =
-        {
-          label: 'Add new task here....',
-          inputValue: '',
-          error: false
-        };
+const InputItem = (
+  { items, onClickAdd }
+) => {
+  const InitialState = {
+    label: 'Add new task here....',
+    inputValue: '',
+    error: false
+  };
+  const [inputItem, setInputItem] = useState(InitialState);
 
-    onInputHandler = ({ items, onClickAdd }) => {
-      if (!(/[a-zA-Zа-яА-Я1-9]+/.test(this.state.inputValue))) {
-        this.setState({
+  const onInputHandler = () => {
+    if (!(/[a-zA-Zа-яА-Я1-9]+/.test(inputItem.inputValue))) {
+      setInputItem({
+        inputValue: '',
+        error: true,
+        label: 'Insert valid text'
+      });
+      return;
+    }
+
+    for (let i = 0; i < items.length; i += 1) {
+      if (inputItem.inputValue === items[i].value) {
+        setInputItem({
           inputValue: '',
           error: true,
-          label: 'Insert valid text'
+          label: 'This task already exists'
         });
         return;
       }
+    }
 
-      for (let i = 0; i < items.length; i += 1) {
-        if (this.state.inputValue === items[i].value) {
-          this.setState({
-            inputValue: '',
-            error: true,
-            label: 'This task already exists'
-          });
-          return;
-        }
-      }
+    onClickAdd(inputItem.inputValue);
+    setInputItem({ inputValue: '', label: 'Add new task here....' });
+  };
 
-
-      onClickAdd(this.state.inputValue);
-
-      this.setState({ inputValue: '', label: 'Add new task here....' });
-    };
-
-    render() {
-      const { items, onClickAdd } = this.props;
-
-      return (
+  return (
             <form className={styles.input}
-                  onSubmit={() => this.onInputHandler({ items, onClickAdd })}>
+                  onSubmit={() => onInputHandler(items, onClickAdd)}>
                 <TextField
                     type="form"
                     id="filled-basic"
-                    label={this.state.label}
+                    label={InputItem.label}
                     variant="filled"
                     className={styles.text}
-                    error={this.state.error}
-                    value={this.state.inputValue}
-                    onChange={event => this.setState(
+                    error={inputItem.error}
+                    value={inputItem.inputValue}
+                    onChange={event => setInputItem(
                       {
                         inputValue: event.target.value.toUpperCase(),
                         error: false
@@ -64,14 +61,13 @@ class InputItem extends React.Component {
                 <Button
                     variant="outlined"
                     size="large"
-                    onClick={() => this.onInputHandler({ items, onClickAdd })}
+                    onClick={() => onInputHandler({ items, onClickAdd })}
                 >
                     Add
                 </Button>
             </form>
-      );
-    }
-}
+  );
+};
 
 InputItem.propTypes = {
   items: propTypes.array.isRequired,

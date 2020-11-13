547b262145c002ecffb0833d30d87a927a79ae1b
Natalya Myunster <natalyamyunster@yahoo.com>
2020-11-13 12:02:43 +0100

    started change styles with mentor 13.11.20 (1)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,14 +10,16 @@ import styles from './App.module.css';
 const App = () => (
     <Router>
         <div className={styles.App}>
-            <div className={styles.wrap}>
+            <div className={styles.nav}>
                 <Card className={styles.sidebar}>
-                    <MenuList>
+                    <MenuList className={styles.menuList}>
                         <Link to='/' className={styles.link}><MenuItem>ABOUT</MenuItem></Link>
                         <Link to='/todo' className={styles.link}><MenuItem>TODO_LIST</MenuItem></Link>
-                        <Link to='/contacts' className={styles.link}> <MenuItem>CONTACTS</MenuItem></Link>
+                        <Link to='/contacts' className={styles.link}> <MenuItem>CV</MenuItem></Link>
                     </MenuList>
                 </Card>
+            </div>
+            <div className={styles.wrap}>
                 <Card className={styles.content}>
                     <Route path='/' exact component={About} />
                     <Route path='/todo' component={Todo} />
